9a68ff484f532aaa86fa9d62b77e0dbf9de84406
Sephoelle <gerasimov.anton97@gmail.com>
2025-01-09 22:23:25 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'Антон'
+title = 'Сходить в магазин'
+content = 'купить: масло, хлеб, молоко, чай!'
+status = 'не выполнено'
+create_date = '09-01-2025'
+issue_date = '10-01-2025'
+
+print('Имя пользователя:', username)
+print('Заголовок:', title)
+print('Описание:', content)
+print('Статус:', status)
+print('Дата создания:', create_date)
+print('Дата истечения:', issue_date)
