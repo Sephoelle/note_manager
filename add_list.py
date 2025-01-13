4f53a1515564d80fef7d84e074d1eadb05b94b10
Sephoelle <gerasimov.anton97@gmail.com>
2025-01-13 17:47:11 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,17 @@
+username = input('Введите имя: ')
+title1 = input('Введите заголовок: ')
+title2 = input('Введите заголовок: ')
+title3 = input('Введите заголовок: ')
+title_list = [title1.capitalize(), title2.capitalize(), title3.capitalize()]
+content = input('Введите содержание заметки: ')
+status = input('Статус заметки: ')
+create_date = str(input('Дата создания заметки в формате "дд.мм.гггг": '))
+issue_date = str(input('Дата истечения заметки в формате "дд.мм.гггг": '))
+
+print('Имя пользователя:', username)
+print('Заголовок:', end=' ')
+print(*title_list, sep='. ')
+print('Описание:', content)
+print('Статус:', status)
+print('Дата создания:', create_date[:5])
+print('Дата истечения:', issue_date[:5])
