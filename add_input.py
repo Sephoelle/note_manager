12194052d11a5cdffc1364fb909d36a692f25bb1
Sephoelle <gerasimov.anton97@gmail.com>
2025-01-15 18:45:31 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,13 @@
+username = input('Введите имя: ')
+title = input('Введите заголовок: ')
+content = input('Введите содержание заметки: ')
+status = input('Статус заметки: ')
+create_date = str(input('Дата создания заметки в формате "дд.мм.гггг": '))
+issue_date = str(input('Дата истечения заметки в формате "дд.мм.гггг": '))
+
+print('Имя пользователя:', username)
+print('Заголовок:', title)
+print('Описание:', content)
+print('Статус:', status)
+print('Дата создания:', create_date[:5])
+print('Дата истечения:', issue_date[:5])
---
+++ final2.py
@@ -0,0 +1,15 @@
+note = []
+title_list = []
+
+note.append(input('Введите имя: '))
+
+title_list.append(input('Введите заголовок: '))
+title_list.append(input('Введите заголовок: '))
+title_list.append(input('Введите заголовок: '))
+note.append(title_list)
+note.append(input('Введите содержание заметки: '))
+note.append(input('Статус заметки: '))
+note.append(input('Дата создания заметки в формате "дд.мм.гггг": '))
+note.append(input('Дата истечения заметки в формате "дд.мм.гггг": '))
+
+print(note)
